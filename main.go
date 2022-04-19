d6734d82066e67cef00705924c7b4c011acc5e10
russellemergentai <russell.emergentai@gmail.com>
2022-04-19 16:23:06 +0100

    Update main.go

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ func main() {
 	// docker image ls
 	// docker run --publish 8080:8080 beta
 	// http://localhost:8080/users/bob
+
+	// az aks get-credentials --resource-group K8SRG --name aksbetatest
+	// kubectl apply -f deploy.yml
+	// curl http://20.108.239.131:30475/users/bob
+	//  kubectl logs web-65f9f75fdf-dccct
+	//
 	greet.Hello()
 	webstuff.IDWebcall()
 }
